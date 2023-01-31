c5fc4e8874e6c7ad31a12c11ace62bba94de1968
nguyễn lan anh <loglamo@gmail.com>
2023-01-31 16:20:50 +0900

    Update Producer.py

--- SRC/kafka streaming/Producer.py
+++ SRC/kafka streaming/Producer.py
@@ -1,41 +1,31 @@
-import json
-import sys, getopt
-import time
-from confluent_kafka import Producer
-import logging
-logging.basicConfig(level=logging.DEBUG)
-#Get Command Line Arguments
-def func(argv):
-    input_file = ''
-    try:
-        opts, args = getopt.getopt(argv,"hi:",["ifile="])
-    except getopt.GetoptError:
-        print 'producer.py -i <path to json inputfile>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'parser.py -i <path to json inputfile>
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            input_file = arg
-    data = read_json(input_file)
-    produce(data)
-def read_json(file):
-    with open(file, 'r') as f:
-        data = json.load(f)
-    return data
-def delivery_callback(err, msg):
-    if err:
-        sys.stderr.write('%% Message failed delivery: %s\n' % err)
-    else:
-        sys.stderr.write('%% Message delivered to %s [%d] @ %d\n' %
-                (msg.topic(), msg.partition(), msg.offset()))
-def serialize(item):
-    return json.dumps(item, default=lambda o: o.__dict__)
-def produce(data):
-    conf = {'bootstrap.servers': 'localhost:29094'}
-    p = Producer(**conf)
-    for item in data:
-        p.produce('log-input', value=serialize(item), callback=delivery_callback)
-if __name__ == "__main__":
-    func(sys.argv[1:])
+from kafka import KafkaProducer
+import jsons
+
+class MessageProducer:
+      broker = ""
+      topic = ""
+      producer = None
+
+      def __init__(self, broker, topic):
+          self.broker = broker
+          self.topic = topic
+          self.producer = KafkaProducer(bootstrap_servers=self.broker, value_serializer=lambda v: jsons.dumps(v).encode('utf-8'), acks='all', retries = 3)
+
+      def send_msg(self,msg):
+          print("sending message ...")
+          try:
+              future = self.producer.send(self.topic,msg)
+              self.producer.flush()
+              future.get(timeout=60)
+              print("message done ...")
+              return {'status_code':200, 'error':None}
+          except Exception as ex:
+              return ex
+
+broker = '165.194.35.81:9092'
+topic = 'testing'
+message_producer = MessageProducer(broker,topic)
+
+data = {'id':'test1', 'data':'lalalala from cluster2'}
+resp = message_producer.send_msg(data)
+print(resp)
