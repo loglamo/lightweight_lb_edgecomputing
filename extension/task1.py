b5b2d1c89b3fe7b8491e1bb20746216a5263ecaa
Lan Anh Nguyen <loglamo@gmail.com>
2023-06-18 05:43:32 +0900

    Create task1.py

---
+++ extension/task1.py
@@ -0,0 +1,12 @@
+import time                                                                                                                                                                                                
+                                                                                                                                                                                                           
+def readFileWithDelay():                                                                                                                                                                                   
+    fileDir = "/home/syslab/workspace_la/edge_nodes/tasks/reading_data.txt"                                                                                                                                
+    fileReading = open(fileDir, 'r')                                                                                                                                                                       
+    lines = fileReading.readlines()                                                                                                                                                                        
+    for line in lines:                                                                                                                                                                                     
+        time.sleep(3)                                                                                                                                                                                      
+        print(line)                                                                                                                                                                                        
+    fileReading.close()                                                                                                                                                                                    
+                                                                                                                                                                                                           
+readFileWithDelay()                                                                                                                                                                                                   
